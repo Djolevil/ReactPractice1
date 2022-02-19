5d79e9a9cf5b1d3a5683cb57615136886ed5c9d1
Djordje Knezevic <starwarrior@mycity.rs>
2022-02-19 18:06:07 +0100

    Upgrade ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+  return (
+    <div>
+      <div>February 19th 2022</div>
+      <div>
+        <h2>Dog Food</h2>
+        <div>$90</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
