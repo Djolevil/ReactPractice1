a4592aeb6658d483da3804630e2c28693310c1d6
Djordje Knezevic <starwarrior@mycity.rs>
2022-02-19 19:47:35 +0100

    Upgrade ExpenseItem component to output dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 1, 19);
+    const expenseTitle = 'Dog Food';
+    const expenseAmount = 90;
+
+
   return (
     <div className='expense-item'>
-      <div>February 19th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Dog Food</h2>
-        <div className='expense-item__price'>$90</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
