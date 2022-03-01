e67a1e5388a6eade904e3d0f65900a4b6b2c8b1f
Djordje Knezevic <starwarrior@mycity.rs>
2022-03-01 16:13:12 +0100

    Add child to parent component communication

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ const App = () => {
     { id: 'e3', title: 'Light Bulbs', amount: 30, date: new Date(2022, 1, 21) },
     { id: 'e4', title: 'Fruit', amount: 32, date: new Date(2022, 1, 22) },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
