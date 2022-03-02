aebd2e041d24688252c949f0035bbabdfdaf2125
Djordje Knezevic <starwarrior@mycity.rs>
2022-03-02 23:02:55 +0100

    Use stateful list to store expenses

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { useState } from 'react';
+
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  { id: 'e1', title: 'Dog Food', amount: 90, date: new Date(2022, 1, 19) },
+  { id: 'e2', title: 'Milk', amount: 20, date: new Date(2022, 1, 20) },
+  { id: 'e3', title: 'Light Bulbs', amount: 30, date: new Date(2022, 1, 21) },
+  { id: 'e4', title: 'Fruit', amount: 32, date: new Date(2022, 1, 22) },
+];
+
 const App = () => {
-  const expenses = [
-    { id: 'e1', title: 'Dog Food', amount: 90, date: new Date(2022, 1, 19) },
-    { id: 'e2', title: 'Milk', amount: 20, date: new Date(2022, 1, 20) },
-    { id: 'e3', title: 'Light Bulbs', amount: 30, date: new Date(2022, 1, 21) },
-    { id: 'e4', title: 'Fruit', amount: 32, date: new Date(2022, 1, 22) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
